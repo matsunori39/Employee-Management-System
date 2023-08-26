f7f6f0dac0b362052b861a568580f769ee93233a
Noriyuki Matsumoto <matsunori39@gmail.com>
2023-08-26 11:22:26 +0900

    [add]refs #32 'public/css, images' folder prepared and 'mvp.css' added for css

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   // res.send('Hello World!');
   res.render('hello.ejs');
+});
+
+app.get('/top', (req, res) => {
+  res.render('top.ejs');
 });
 
 app.listen(port, ()=> {
