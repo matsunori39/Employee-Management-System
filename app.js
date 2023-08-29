bea732bb5fe5a52e210e32a5ebd524cb2b11627e
Noriyuki Matsumoto <matsunori39@gmail.com>
2023-08-29 10:43:34 +0900

    [update] #40 Changed to 'mysql2' and the DB can be referenced

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(express.static('public'));
 
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'R00tMysq1',
+  database: 'employee'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
 app.get('/', (req, res) => {
-  // res.send('Hello World!');
-  res.render('hello.ejs');
+  connection.query(
+    'SELECT * FROM basics',
+    (error, results) => {
+      console.log(results);
+      res.render('hello.ejs');
+    }
+  );
 });
 
 app.get('/top', (req, res) => {
   res.render('top.ejs');
 });
 
-app.listen(port, ()=> {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(3000);
