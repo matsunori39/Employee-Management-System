da43ebae9020596b762f15690e735f9efc5a8f61
Noriyuki Matsumoto <matsunori39@gmail.com>
2023-10-11 11:31:56 +0900

    Refactor: #129 All routes in app.js to routes/

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 // For support of MySQL 8.0's default authentication method (caching_sha2_password) in Node.js, use 'mysql2'
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const app = express();
 // const port = 3000;
+
+const routes = require('./routes/index');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
@@ -16,17 +18,13 @@ app.use(
   })
 );
 
-const connection = mysql.createConnection({
-  // host: '127.0.0.1',  // Change from 'localhost'
-  host: 'employee-management-system-mysql-1',  // Change from 'localhost'
-  user: 'root',
-  password: 'R00tMysq1',
-  database: 'employee'
-});
-
-let isSearched = false;
-let searchWord = '';
-let isBeforeSignup = false;
+// const connection = mysql.createConnection({
+//   // host: '127.0.0.1',  // Change from 'localhost'
+//   host: 'employee-management-system-mysql-1',  // Change from 'localhost'
+//   user: 'root',
+//   password: 'R00tMysq1',
+//   database: 'employee'
+// });
 
 app.use((req, res, next) => {
   if (req.session.userId === undefined) {
@@ -38,242 +36,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  isBeforeSignup = false;
-  if (res.locals.isLoggedIn) {
-    return res.redirect('/list');
-  }
-  res.render('login.ejs');
-});
-
-app.get('/list', (req, res, next) => {
-  if (!res.locals.isLoggedIn) {
-    return res.redirect('/');
-  }
-  connection.query(
-    'SELECT * FROM basics',
-    (error, results) => {
-      res.render('index.ejs', {items: results});
-    }
-  );
-  isSearched = false;
-});
-
-app.get('/new', (req, res) => {
-  if (!res.locals.isLoggedIn) {
-    return res.redirect('/');
-  }
-  res.render('new.ejs');
-});
-
-app.post('/create', (req, res) => {
-  connection.query(
-    'INSERT INTO basics (name, department) VALUES (?, ?)',
-    [ req.body.employeeName, req.body.departmentName ],
-    (error, results) => {
-      res.redirect('/list');
-    }
-  );
-});
-
-app.get('/edit/:id', (req, res) => {
-  if (!res.locals.isLoggedIn) {
-    return res.redirect('/');
-  }
-  connection.query(
-    'SELECT * FROM basics WHERE id = ?',
-    [req.params.id],
-    (error, results) => {
-      res.render('edit.ejs', {item: results[0]});
-    }
-  );
-});
-
-app.post('/update/:id', (req, res) => {
-  connection.query(
-    'UPDATE basics SET name = ?, department = ? WHERE id = ?',
-    [ req.body.itemName, req.body.itemDepartment, req.params.id ],
-    (error, results) => {
-      if (isSearched) {
-        const query = 'SELECT * FROM basics WHERE name LIKE ?';
-        const likeWord = '%' + searchWord + '%';
-        connection.query(
-          query,
-          [ likeWord ],
-          (error, results) => {
-            res.render('search.ejs', {items: results, word: searchWord});
-          }
-        );
-      } else {
-        res.redirect('/list');
-      }
-    }
-  );
-});
-
-app.post('/search', (req, res) => {
-  searchWord = req.body.searchName;
-  const query = 'SELECT * FROM basics WHERE name LIKE ?';
-  const likeWord = '%' + searchWord + '%';
-  connection.query(
-    query,
-    [ likeWord ],
-    (error, results) => {
-      res.render('search.ejs', {items: results, word: searchWord});
-    }
-  );
-  isSearched = true;
-});
-
-app.get('/fromEdit', (req, res) => {
-  if (isSearched) {
-    const query = 'SELECT * FROM basics WHERE name LIKE ?';
-    const likeWord = '%' + searchWord + '%';
-    connection.query(
-      query,
-      [ likeWord ],
-      (error, results) => {
-        res.render('search.ejs', {items: results, word: searchWord});
-      }
-    );
-  } else {
-    res.redirect('/list');
-  }
-});
-
-app.post('/delete/:id', (req, res) => {
-  connection.query(
-    'DELETE FROM basics WHERE id = ?',
-    [ req.params.id ],
-    (error, results) => {
-      res.redirect('/fromEdit');
-    }
-  );
-});
-
-app.get('/before-signup', (req, res) => {
-  if (res.locals.isLoggedIn) {
-    return res.redirect('/');
-  }
-  isBeforeSignup = true;
-  res.render('before-signup.ejs');
-});
-
-app.get('/signup', (req, res) => {
-  if (!res.locals.isLoggedIn) {
-    return res.redirect('/before-signup');
-  }
-  res.render('signup.ejs', {errors: [] });
-});
-
-app.post('/signup',
-  (req, res, next) => {
-    console.log('Empty check of input values');
-
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const errors = [];
-
-    if (username === '') {
-      errors.push('- ユーザー名が空です');
-    }
-    if (email === '') {
-      errors.push('- メールアドレスが空です');
-    }
-    if (password.length < 6) {
-      errors.push('- パスワードは6文字以上で入力して下さい');
-    }
-
-    if (errors.length > 0) {
-      res.render('signup.ejs', { errors: errors });
-    } else {
-      next();
-    }
-  },
-  (req, res, next) => {
-    console.log('Check duplicate email addresses');
-    const email = req.body.email;
-    const errors = [];
-    connection.query(
-      'SELECT * FROM users WHERE email=?',
-      [email],
-      (error, results) => {
-        if (results.length > 0) {
-          errors.push('- 入力されたメールアドレスは登録済みです');
-          res.render('signup.ejs', { errors: errors});
-        } else {
-          next();
-        }
-      }
-    );
-  },
-  (req, res) => {
-    console.log('User registration');
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    bcrypt.hash(password, 10, (error, hash) => {
-      connection.query(
-        'INSERT INTO users(username, email, password) VALUES (?, ?, ?)',
-        [username, email, hash],
-        (error, results) => {
-          req.session.userId = results.insertId;
-          req.session.username = username;
-          res.redirect('/list');
-        }
-      );
-    });
-  }
-);
-
-app.post('/login', (req, res) => {
-  const email = req.body.email;
-  connection.query(
-    'SELECT * FROM users WHERE email = ?',
-    [email],
-    (error, results) => {
-      if (results.length > 0) {
-        const plain = req.body.password;
-        const hash = results[0].password;
-        bcrypt.compare(plain, hash, (error, isEqual) => {
-          if (isEqual) {
-            req.session.userId = results[0].id;
-            req.session.username = results[0].username;
-            if (isBeforeSignup) {
-              isBeforeSignup = false;
-              res.redirect('/signup');
-            } else {
-              res.redirect('/list');
-            }
-          } else {
-            // !isEqual
-            if (isBeforeSignup) {
-              res.redirect('/before-signup');
-            } else {
-              res.redirect('/');
-            }
-          }
-        });
-      } else {
-        // no results
-        if (isBeforeSignup) {
-          isBeforeSignup = false;
-          res.redirect('/before-signup');
-        } else {
-          res.redirect('/');
-        }
-      }
-    }
-  );
-});
-
-app.get('/logout', (req, res) => {
-  req.session.destroy((error) => {
-    res.redirect('/');
-  });
-});
+app.use('/', routes);
 
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!");
